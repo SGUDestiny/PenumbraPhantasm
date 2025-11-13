f75e52cbafa48df266dc72040b4206c6f91c03c4
SGUDestiny <panormov2006@gmail.com>
2025-11-14 03:13:23 +0400

    Started working on actual Dark Fountains

--- src/main/java/destiny/penumbra_phantasm/server/fountain/DarkFountainCapability.java
+++ src/main/java/destiny/penumbra_phantasm/server/fountain/DarkFountainCapability.java
@@ -1,16 +1,51 @@
 package destiny.penumbra_phantasm.server.fountain;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.common.util.INBTSerializable;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class DarkFountainCapability implements INBTSerializable<CompoundTag> {
+
+    private static final String DARK_FOUNTAINS = "dark_fountains";
+
+    public HashMap<UUID, DarkFountain> darkFountains = new HashMap<>();
+
     @Override
     public CompoundTag serializeNBT() {
-        return null;
+        CompoundTag tag = new CompoundTag();
+
+        tag.put(DARK_FOUNTAINS, serializeDarkFountains());
+
+        return tag;
+    }
+
+    private CompoundTag serializeDarkFountains() {
+        CompoundTag objectsTag = new CompoundTag();
+
+        this.darkFountains.forEach((uuid, pad) -> {
+            objectsTag.put(uuid.toString(), pad.save());
+        });
+
+        return objectsTag;
+    }
+
+    public void addRocketPad(UUID uuid, BlockPos fountainPos, ResourceKey<Level> fountainDimension, BlockPos destinationPos, ResourceKey<Level> destinationDimension, int animationTick) {
+        this.darkFountains.put(uuid, new DarkFountain(fountainPos, fountainDimension, destinationPos, destinationDimension, animationTick));
     }
 
     @Override
-    public void deserializeNBT(CompoundTag compoundTag) {
+    public void deserializeNBT(CompoundTag tag) {
+        deserializeDarkFountains(tag.getCompound(DARK_FOUNTAINS));
+    }
 
+    private void deserializeDarkFountains(CompoundTag tag) {
+        for(String key : tag.getAllKeys()) {
+            this.darkFountains.put(UUID.fromString(key), DarkFountain.load(tag.getCompound(key)));
+        }
     }
 }
