6fc1f2e9c4d5dffc2cd0f04892eb1aee7042e6ad
SGUDestiny <panormov2006@gmail.com>
2026-01-29 22:17:00 +0400

    Some intro screen tinkering

--- src/main/java/destiny/penumbra_phantasm/client/render/screen/IntroScreen.java
+++ src/main/java/destiny/penumbra_phantasm/client/render/screen/IntroScreen.java
@@ -12,12 +12,13 @@ import net.minecraft.resources.ResourceLocation;
 
 public class IntroScreen extends Screen {
     Minecraft minecraft = Minecraft.getInstance();
+    public static final ResourceLocation BLACK_SCREEN = new ResourceLocation(PenumbraPhantasm.MODID, "textures/misc/black_screen.png");
     public static final ResourceLocation IMAGE_DEPTH = new ResourceLocation(PenumbraPhantasm.MODID, "textures/misc/image_depth_blue.png");
     public static final ResourceLocation BLURRY_SOUL = new ResourceLocation(PenumbraPhantasm.MODID, "textures/misc/blurry_soul.png");
     public final Runnable onFinished;
-    public int tick = 0;
     public int screenLength = 40 * 20;
     public int droneLength = (int) (8.727 * 20);
+    public int tick = droneLength;
 
     public IntroScreen(Runnable runnable) {
         super(GameNarrator.NO_TITLE);
@@ -30,27 +31,34 @@ public class IntroScreen extends Screen {
             this.closeScreen();
         } else {
             if (tick < droneLength * 3) {
-                minecraft.player.playSound(SoundRegistry.INTRO_DRONE.get());
-            } else {
-                if (tick == droneLength * 3) {
-                    minecraft.player.playSound(SoundRegistry.INTRO_ANOTHER_HIM.get());
+                if (tick % droneLength == 0) {
+                    minecraft.player.playSound(SoundRegistry.INTRO_DRONE.get());
                 }
+            } else if (tick == droneLength * 3) {
+                minecraft.player.playSound(SoundRegistry.INTRO_ANOTHER_HIM.get());
             }
             tick++;
         }
     }
 
     @Override
-    public void render(GuiGraphics graphics, int p_281550_, int p_282878_, float p_282465_) {
+    public void renderBackground(GuiGraphics graphics) {
+        graphics.pose().pushPose();
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR);
         if (this.tick > droneLength * 3) {
-            graphics.pose().pushPose();
-            RenderSystem.enableBlend();
-            RenderSystem.blendFunc(GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR);
             graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0.0F, 0.0F, this.width, this.height, this.width, this.height);
-            RenderSystem.disableBlend();
-            RenderSystem.defaultBlendFunc();
-            graphics.pose().popPose();
+        } else {
+            graphics.blit(BLACK_SCREEN, 0, 0, 0, 0.0F, 0.0F, this.width, this.height, this.width, this.height);
         }
+        RenderSystem.disableBlend();
+        RenderSystem.defaultBlendFunc();
+        graphics.pose().popPose();
+        super.renderBackground(graphics);
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int p_281550_, int p_282878_, float p_282465_) {
         super.render(graphics, p_281550_, p_282878_, p_282465_);
     }
 
