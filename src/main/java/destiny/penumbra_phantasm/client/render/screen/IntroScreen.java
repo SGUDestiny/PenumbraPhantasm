e63d62af0bc4c589fe63ac057d5c9511b6d6b1a4
SGUDestiny <panormov2006@gmail.com>
2026-02-04 22:47:16 +0400

    Kinda works but not

--- src/main/java/destiny/penumbra_phantasm/client/render/screen/IntroScreen.java
+++ src/main/java/destiny/penumbra_phantasm/client/render/screen/IntroScreen.java
@@ -1,18 +1,13 @@
 package destiny.penumbra_phantasm.client.render.screen;
 
-import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import destiny.penumbra_phantasm.PenumbraPhantasm;
 import destiny.penumbra_phantasm.server.registry.SoundRegistry;
 import net.minecraft.client.GameNarrator;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 
@@ -22,9 +17,16 @@ public class IntroScreen extends Screen {
     public static final ResourceLocation IMAGE_DEPTH = new ResourceLocation(PenumbraPhantasm.MODID, "textures/misc/image_depth_blue_alt.png");
     public static final ResourceLocation BLURRY_SOUL = new ResourceLocation(PenumbraPhantasm.MODID, "textures/misc/blurry_soul.png");
     public final Runnable onFinished;
-    public int screenLength = 60 * 20;
+    public int screenLength = 120 * 20;
     public int droneLength = (int) (8.727 * 20);
     public int tick = droneLength;
+    //Depths lifetime - 8 seconds
+    public float depthsLifetime = 8 * 20;
+    public float depthsTick1 = 0;
+    public float depthsTick2 = depthsLifetime / 5;
+    public float depthsTick3 = (depthsLifetime / 5) * 2;
+    public float depthsTick4 = (depthsLifetime / 5) * 3;
+    public float depthsTick5 = (depthsLifetime / 5) * 4;
 
     public int depthsStart = droneLength * 5;
 
@@ -51,10 +53,45 @@ public class IntroScreen extends Screen {
                 if (tick % droneLength == 0 || tick == 0) {
                     minecraft.player.playSound(SoundRegistry.INTRO_DRONE.get());
                 }
-            } else if (tick == depthsStart) {
+            }
+            if (tick == depthsStart) {
                 minecraft.player.playSound(SoundRegistry.INTRO_ANOTHER_HIM.get());
             }
+            if (tick > depthsStart) {
+                if (depthsTick1 >= depthsLifetime) {
+                    depthsTick1 = 0;
+                } else {
+                    depthsTick1++;
+                }
+                if (depthsTick2 >= depthsLifetime) {
+                    depthsTick2 = 0;
+                } else {
+                    depthsTick2++;
+                }
+                if (depthsTick3 >= depthsLifetime) {
+                    depthsTick3 = 0;
+                } else {
+                    depthsTick3++;
+                }
+                if (depthsTick4 >= depthsLifetime) {
+                    depthsTick4 = 0;
+                } else {
+                    depthsTick4++;
+                }
+                if (depthsTick5 >= depthsLifetime) {
+                    depthsTick5 = 0;
+                } else {
+                    depthsTick5++;
+                }
+            }
             tick++;
+
+            System.out.println("---");
+            System.out.println("depthsTick1: "+ depthsTick1);
+            System.out.println("depthsTick2: "+ depthsTick2);
+            System.out.println("depthsTick3: "+ depthsTick3);
+            System.out.println("depthsTick4: "+ depthsTick4);
+            System.out.println("depthsTick5: "+ depthsTick5);
         }
     }
 
@@ -103,14 +140,90 @@ public class IntroScreen extends Screen {
         }
 
         if (this.tick > depthsStart) {
+            //BG Depths
             pose.pushPose();
+            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, 1);
             //atlasLocation, x, y, blitOffset, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, 1f);
-            RenderSystem.setShaderTexture(0, IMAGE_DEPTH);
             graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            RenderSystem.setShaderColor(1, 1, 1, 1);
             pose.popPose();
 
+            //Depths layer 1
+            float depthsAlphaDelta1 = depthsTick1 / depthsLifetime;
+            float depthsAlpha1 = Mth.lerp(depthsAlphaDelta1, 1f, 0f);
+            float depthsSize1 = Mth.lerp(depthsAlphaDelta1, 0f, 1f);
+
+            pose.pushPose();
+            pose.translate(this.width / 2f, this.height / 2f, 0);
+            pose.scale(1 + depthsSize1, 1 + depthsSize1, 1);
+            pose.translate(-this.width / 2f, -this.height / 2f, 0);
+            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, depthsAlpha1);
+            //atlasLocation, x, y, blitOffset, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight
+            graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            RenderSystem.setShaderColor(1, 1, 1, 1);
+            pose.popPose();
+
+            //Depths layer 2
+            float depthsAlphaDelta2 = depthsTick2 / depthsLifetime;
+            float depthsAlpha2 = Mth.lerp(depthsAlphaDelta2, 1f, 0f);
+            float depthsSize2 = Mth.lerp(depthsAlphaDelta2, 0f, 1f);
+
+            pose.pushPose();
+            pose.translate(this.width / 2f, this.height / 2f, 0);
+            pose.scale(1 + depthsSize2, 1 + depthsSize2, 1);
+            pose.translate(-this.width / 2f, -this.height / 2f, 0);
+            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, depthsAlpha2);
+            //atlasLocation, x, y, blitOffset, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight
+            graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            RenderSystem.setShaderColor(1, 1, 1, 1);
+            pose.popPose();
+
+            //Depths layer 3
+            float depthsAlphaDelta3 = depthsTick3 / depthsLifetime;
+            float depthsAlpha3 = Mth.lerp(depthsAlphaDelta3, 1f, 0f);
+            float depthsSize3 = Mth.lerp(depthsAlphaDelta3, 0f, 1f);
+
+            pose.pushPose();
+            pose.translate(this.width / 2f, this.height / 2f, 0);
+            pose.scale(1 + depthsSize3, 1 + depthsSize3, 1);
+            pose.translate(-this.width / 2f, -this.height / 2f, 0);
+            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, depthsAlpha3);
+            //atlasLocation, x, y, blitOffset, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight
+            graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            RenderSystem.setShaderColor(1, 1, 1, 1);
+            pose.popPose();
+
+            //Depths layer 4
+            float depthsAlphaDelta4 = depthsTick4 / depthsLifetime;
+            float depthsAlpha4 = Mth.lerp(depthsAlphaDelta4, 1f, 0f);
+            float depthsSize4 = Mth.lerp(depthsAlphaDelta4, 0f, 1f);
+
+            pose.pushPose();
+            pose.translate(this.width / 2f, this.height / 2f, 0);
+            pose.scale(1 + depthsSize4, 1 + depthsSize4, 1);
+            pose.translate(-this.width / 2f, -this.height / 2f, 0);
+            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, depthsAlpha4);
+            //atlasLocation, x, y, blitOffset, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight
+            graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            RenderSystem.setShaderColor(1, 1, 1, 1);
+            pose.popPose();
+
+            //Depths layer 5
+            float depthsAlphaDelta5 = depthsTick5 / depthsLifetime;
+            float depthsAlpha5 = Mth.lerp(depthsAlphaDelta5, 1f, 0f);
+            float depthsSize5 = Mth.lerp(depthsAlphaDelta5, 0f, 1f);
+
+            pose.pushPose();
+            pose.translate(this.width / 2f, this.height / 2f, 0);
+            pose.scale(1 + depthsSize5, 1 + depthsSize5, 1);
+            pose.translate(-this.width / 2f, -this.height / 2f, 0);
+            RenderSystem.setShaderColor(depthsAlpha, depthsAlpha, depthsAlpha, depthsAlpha5);
+            //atlasLocation, x, y, blitOffset, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight
+            graphics.blit(IMAGE_DEPTH, 0, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+            RenderSystem.setShaderColor(1, 1, 1, 1);
+            pose.popPose();
+
+            //Black bars
             pose.pushPose();
             graphics.blit(BLACK_SCREEN, 0, 0, 0, 0.0F, 0.0F, this.width / 8, this.height, this.width, this.height);
             pose.popPose();
@@ -131,7 +244,7 @@ public class IntroScreen extends Screen {
     @Override
     public void onClose()
     {
-
+        this.onFinished.run();
     }
     public void closeScreen() {
         this.onFinished.run();
