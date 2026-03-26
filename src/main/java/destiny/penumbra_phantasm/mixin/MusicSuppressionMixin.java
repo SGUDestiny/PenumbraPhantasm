2a13efe64ccd24bbca4319a570d38697caea9a71
SGUDestiny <panormov2006@gmail.com>
2026-03-26 07:02:14 +0400

    Removed static field from music suppression mixin, caused crash on load

--- src/main/java/destiny/penumbra_phantasm/mixin/MusicSuppressionMixin.java
+++ src/main/java/destiny/penumbra_phantasm/mixin/MusicSuppressionMixin.java
@@ -6,22 +6,18 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.sounds.Music;
 import net.minecraft.sounds.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
 public class MusicSuppressionMixin {
-    @Unique
-    private static final Music penumbraPhantasm$SILENCE = new Music(SoundEvents.MUSIC_GAME, Integer.MAX_VALUE, Integer.MAX_VALUE, true);
-
     @Inject(method = "getSituationalMusic", at = @At("HEAD"), cancellable = true)
     private void penumbraPhantasm$suppressDarkWorldMusic(CallbackInfoReturnable<Music> cir) {
         Minecraft mc = (Minecraft) (Object) this;
         LocalPlayer player = mc.player;
         if (player != null && DarkFountain.isDarkWorldStatic(player.level().dimension())) {
-            cir.setReturnValue(penumbraPhantasm$SILENCE);
+            cir.setReturnValue(new Music(SoundEvents.MUSIC_GAME, Integer.MAX_VALUE, Integer.MAX_VALUE, true));
         }
     }
 }
