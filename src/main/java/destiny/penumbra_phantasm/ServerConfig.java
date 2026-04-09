ca75404edfc424880cea5314bcf7d09bb5d6dfac
SGUDestiny <panormov2006@gmail.com>
2026-04-09 07:17:40 +0400

    Commented unused config option

--- src/main/java/destiny/penumbra_phantasm/ServerConfig.java
+++ src/main/java/destiny/penumbra_phantasm/ServerConfig.java
@@ -29,14 +29,14 @@ public class ServerConfig {
             .comment("Default: 5")
             .defineInRange("dissipation_rate", 5, 1, 50);
 
-    private static final ForgeConfigSpec.IntValue FOUNTAIN_FIRST_TELEPORT_MIN_RADIUS = BUILDER
+/*    private static final ForgeConfigSpec.IntValue FOUNTAIN_FIRST_TELEPORT_MIN_RADIUS = BUILDER
             .comment("Minimum distance from Dark Fountain to teleport the player when the fountain fills the rooms for the first time. Must be less than maximum")
             .comment("Default: 8")
             .defineInRange("fountain_first_teleport_min_radius", 8, 8, 1048576);
     private static final ForgeConfigSpec.IntValue FOUNTAIN_FIRST_TELEPORT_MAX_RADIUS = BUILDER
             .comment("Minimum distance from Dark Fountain to teleport the player when the fountain fills the rooms for the first time. Must be more than minimum")
             .comment("Default: 16")
-            .defineInRange("fountain_first_teleport_max_radius", 16, 16, 1048576);
+            .defineInRange("fountain_first_teleport_max_radius", 16, 16, 1048576);*/
 
     private static final ForgeConfigSpec.IntValue FOUNTAIN_CONTACT_TELEPORT_MIN_RADIUS = BUILDER
             .comment("Minimum distance from Dark Fountain to teleport the player when they enter fountain's darkness volume. Must be less than maximum")
@@ -62,8 +62,8 @@ public class ServerConfig {
     public static int maxRoomVolume;
     public static int rescanInterval;
     public static int dissipationRate;
-    public static int fountainFirstTeleportMinRadius;
-    public static int fountainFirstTeleportMaxRadius;
+/*    public static int fountainFirstTeleportMinRadius;
+    public static int fountainFirstTeleportMaxRadius;*/
     public static int fountainContactTeleportMinRadius;
     public static int fountainContactTeleportMaxRadius;
     public static int greatDoorPlaceMinRadius;
@@ -78,8 +78,8 @@ public class ServerConfig {
         maxRoomVolume = MAX_ROOM_VOLUME.get();
         rescanInterval = RESCAN_INTERVAL.get();
         dissipationRate = DISSIPATION_RATE.get();
-        fountainFirstTeleportMinRadius = FOUNTAIN_FIRST_TELEPORT_MIN_RADIUS.get();
-        fountainFirstTeleportMaxRadius = FOUNTAIN_FIRST_TELEPORT_MAX_RADIUS.get();
+/*        fountainFirstTeleportMinRadius = FOUNTAIN_FIRST_TELEPORT_MIN_RADIUS.get();
+        fountainFirstTeleportMaxRadius = FOUNTAIN_FIRST_TELEPORT_MAX_RADIUS.get();*/
         fountainContactTeleportMinRadius = FOUNTAIN_CONTACT_TELEPORT_MIN_RADIUS.get();
         fountainContactTeleportMaxRadius = FOUNTAIN_CONTACT_TELEPORT_MAX_RADIUS.get();
         greatDoorPlaceMinRadius = GREAT_DOOR_PLACE_MIN_RADIUS.get();
