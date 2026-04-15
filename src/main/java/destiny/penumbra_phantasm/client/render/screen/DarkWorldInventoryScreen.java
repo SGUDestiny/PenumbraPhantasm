86285e8b5565809ba2c175f1168902314117fca1
SGUDestiny <panormov2006@gmail.com>
2026-04-15 23:56:42 +0400

    Dark world inventory stuff

--- src/main/java/destiny/penumbra_phantasm/client/render/screen/DarkWorldInventoryScreen.java
+++ src/main/java/destiny/penumbra_phantasm/client/render/screen/DarkWorldInventoryScreen.java
@@ -2,25 +2,32 @@ package destiny.penumbra_phantasm.client.render.screen;
 
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import destiny.penumbra_phantasm.PenumbraPhantasm;
+import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.client.gui.screens.recipebook.RecipeUpdateListener;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ClickType;
-import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraft.world.inventory.RecipeBookMenu;
-import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ContainerScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
 import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 
@@ -35,12 +42,22 @@ public class DarkWorldInventoryScreen extends EffectRenderingInventoryScreen<Inv
     private final RecipeBookComponent recipeBookComponent = new RecipeBookComponent();
     private boolean widthTooNarrow;
     private boolean buttonClicked;
+    public Component inventoryLabel = Component.translatable("gui.penumbra_phantasm.dark_world_inventory_title");
+    public Component craftingLabel = Component.translatable("gui.penumbra_phantasm.dark_world_inventory_crafting");
+    public Component equippedLabel = Component.translatable("gui.penumbra_phantasm.dark_world_inventory_equipped");
+    public int equippedLabelX = 57;
+    public int equippedLabelY = 4;
+    public int slotsOffsetX = 0;
+    public int slotsOffsetY = 10;
 
     public DarkWorldInventoryScreen(Player pPlayer) {
         super(pPlayer.inventoryMenu, pPlayer.getInventory(), Component.translatable("container.crafting"));
-        this.titleLabelX = 97;
-        this.imageWidth = 184;
-        this.imageHeight = 180;
+        this.imageWidth = 188;
+        this.imageHeight = 185;
+        this.inventoryLabelX = 57;
+        this.inventoryLabelY = 87;
+        this.titleLabelX = 137;
+        this.titleLabelY = 4;
     }
 
     public void containerTick() {
@@ -58,7 +75,7 @@ public class DarkWorldInventoryScreen extends EffectRenderingInventoryScreen<Inv
         } else {
             super.init();
             this.widthTooNarrow = this.width < 379;
-            this.recipeBookComponent.init(this.width, this.height, this.minecraft, this.widthTooNarrow, (RecipeBookMenu)this.menu);
+            this.recipeBookComponent.init(this.width, this.height, this.minecraft, this.widthTooNarrow, this.menu);
             this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
             this.addRenderableWidget(new ImageButton(this.leftPos + 104, this.height / 2 - 22, 20, 18, 0, 0, 19, RECIPE_BUTTON_LOCATION, (p_289631_) -> {
                 this.recipeBookComponent.toggleVisibility();
@@ -73,36 +90,88 @@ public class DarkWorldInventoryScreen extends EffectRenderingInventoryScreen<Inv
     }
 
     protected void renderLabels(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY) {
-        pGuiGraphics.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 4210752, false);
+        //Equipped label
+        this.drawCenteredString(pGuiGraphics, this.font, this.equippedLabel, this.equippedLabelX, this.equippedLabelY, -1, false);
+        //Crafting label
+        this.drawCenteredString(pGuiGraphics, this.font, this.craftingLabel, this.titleLabelX, this.titleLabelY, -1, false);
+        //Inventory label
+        this.drawCenteredString(pGuiGraphics, this.font, this.inventoryLabel, this.inventoryLabelX, this.inventoryLabelY, -1, false);
     }
 
     public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
-        this.renderBackground(pGuiGraphics);
-        if (this.recipeBookComponent.isVisible() && this.widthTooNarrow) {
-            this.renderBg(pGuiGraphics, pPartialTick, pMouseX, pMouseY);
-            this.recipeBookComponent.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-        } else {
-            this.recipeBookComponent.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-            super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-            this.recipeBookComponent.renderGhostRecipe(pGuiGraphics, this.leftPos, this.topPos, false, pPartialTick);
+        int i = this.leftPos;
+        int j = this.topPos;
+        this.renderBg(pGuiGraphics, pPartialTick, pMouseX, pMouseY);
+        MinecraftForge.EVENT_BUS.post(new ContainerScreenEvent.Render.Background(this, pGuiGraphics, pMouseX, pMouseY));
+        RenderSystem.disableDepthTest();
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().translate((float)i, (float)j, 0.0F);
+        this.hoveredSlot = null;
+
+        for(int k = 0; k < this.menu.slots.size(); ++k) {
+            Slot slot = this.menu.slots.get(k);
+            if (slot.isActive()) {
+                this.renderSlot(pGuiGraphics, slot);
+            }
+
+            if (this.isHovering(slot, pMouseX, pMouseY) && slot.isActive()) {
+                this.hoveredSlot = slot;
+                int x = slot.x + slotsOffsetX - slotsOffsetX;
+                int y = slot.y + slotsOffsetY - slotsOffsetY;
+                if (this.hoveredSlot.isHighlightable()) {
+                    renderSlotHighlight(pGuiGraphics, x, y, 0, this.getSlotColor(k));
+                }
+            }
         }
 
-        this.renderTooltip(pGuiGraphics, pMouseX, pMouseY);
-        this.recipeBookComponent.renderTooltip(pGuiGraphics, this.leftPos, this.topPos, pMouseX, pMouseY);
-        this.xMouse = (float)pMouseX;
-        this.yMouse = (float)pMouseY;
+        this.renderLabels(pGuiGraphics, pMouseX, pMouseY);
+        MinecraftForge.EVENT_BUS.post(new ContainerScreenEvent.Render.Foreground(this, pGuiGraphics, pMouseX, pMouseY));
+        ItemStack itemstack = this.draggingItem.isEmpty() ? this.menu.getCarried() : this.draggingItem;
+        if (!itemstack.isEmpty()) {
+            int l1 = 8;
+            int i2 = this.draggingItem.isEmpty() ? 8 : 16;
+            String s = null;
+            if (!this.draggingItem.isEmpty() && this.isSplittingStack) {
+                itemstack = itemstack.copyWithCount(Mth.ceil((float)itemstack.getCount() / 2.0F));
+            } else if (this.isQuickCrafting && this.quickCraftSlots.size() > 1) {
+                itemstack = itemstack.copyWithCount(this.quickCraftingRemainder);
+                if (itemstack.isEmpty()) {
+                    s = ChatFormatting.YELLOW + "0";
+                }
+            }
+
+            this.renderFloatingItem(pGuiGraphics, itemstack, pMouseX - i - 8, pMouseY - j - i2, s);
+        }
+
+        if (!this.snapbackItem.isEmpty()) {
+            float f = (float)(Util.getMillis() - this.snapbackTime) / 100.0F;
+            if (f >= 1.0F) {
+                f = 1.0F;
+                this.snapbackItem = ItemStack.EMPTY;
+            }
+
+            int j2 = this.snapbackEnd.x - this.snapbackStartX;
+            int k2 = this.snapbackEnd.y - this.snapbackStartY;
+            int j1 = this.snapbackStartX + (int)((float)j2 * f);
+            int k1 = this.snapbackStartY + (int)((float)k2 * f);
+            this.renderFloatingItem(pGuiGraphics, this.snapbackItem, j1, k1, null);
+        }
+
+        pGuiGraphics.pose().popPose();
+        RenderSystem.enableDepthTest();
     }
 
     protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         int i = this.leftPos;
         int j = this.topPos;
         pGuiGraphics.blit(DARK_WORLD_INVENTORY_LOCATION, i, j, 0, 0, this.imageWidth, this.imageHeight);
-        renderEntityInInventoryFollowsMouse(pGuiGraphics, i + 51, j + 75, 30, (float)(i + 51) - this.xMouse, (float)(j + 75 - 50) - this.yMouse, this.minecraft.player);
+        renderEntityInInventoryFollowsMouse(pGuiGraphics, i + 57, j + 81, 30, (float)(i + 57) - this.xMouse, (float)(j + 81 - 50) - this.yMouse, this.minecraft.player);
     }
 
     public static void renderEntityInInventoryFollowsMouse(GuiGraphics pGuiGraphics, int pX, int pY, int pScale, float pMouseX, float pMouseY, LivingEntity pEntity) {
-        float f = (float)Math.atan((double)(pMouseX / 40.0F));
-        float f1 = (float)Math.atan((double)(pMouseY / 40.0F));
+        float f = (float)Math.atan((pMouseX / 40.0F));
+        float f1 = (float)Math.atan((pMouseY / 40.0F));
         renderEntityInInventoryFollowsAngle(pGuiGraphics, pX, pY, pScale, f, f1, pEntity);
     }
 
@@ -130,7 +199,7 @@ public class DarkWorldInventoryScreen extends EffectRenderingInventoryScreen<Inv
 
     public static void renderEntityInInventory(GuiGraphics pGuiGraphics, int pX, int pY, int pScale, Quaternionf pPose, @Nullable Quaternionf pCameraOrientation, LivingEntity pEntity) {
         pGuiGraphics.pose().pushPose();
-        pGuiGraphics.pose().translate((double)pX, (double)pY, (double)50.0F);
+        pGuiGraphics.pose().translate(pX, pY, (double)50.0F);
         pGuiGraphics.pose().mulPoseMatrix((new Matrix4f()).scaling((float)pScale, (float)pScale, (float)(-pScale)));
         pGuiGraphics.pose().mulPose(pPose);
         Lighting.setupForEntityInInventory();
@@ -141,7 +210,7 @@ public class DarkWorldInventoryScreen extends EffectRenderingInventoryScreen<Inv
         }
 
         entityrenderdispatcher.setRenderShadow(false);
-        RenderSystem.runAsFancy(() -> entityrenderdispatcher.render(pEntity, (double)0.0F, (double)0.0F, (double)0.0F, 0.0F, 1.0F, pGuiGraphics.pose(), pGuiGraphics.bufferSource(), 15728880));
+        RenderSystem.runAsFancy(() -> entityrenderdispatcher.render(pEntity, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, pGuiGraphics.pose(), pGuiGraphics.bufferSource(), 15728880));
         pGuiGraphics.flush();
         entityrenderdispatcher.setRenderShadow(true);
         pGuiGraphics.pose().popPose();
@@ -190,6 +259,62 @@ public class DarkWorldInventoryScreen extends EffectRenderingInventoryScreen<Inv
 
     @Override
     public void renderSlot(GuiGraphics pGuiGraphics, Slot pSlot) {
-        super.renderSlot(pGuiGraphics, pSlot);
+        int x = pSlot.x - slotsOffsetX;
+        int y = pSlot.y - slotsOffsetY;
+        ItemStack itemstack = pSlot.getItem();
+        boolean flag = false;
+        boolean flag1 = pSlot == this.clickedSlot && !this.draggingItem.isEmpty() && !this.isSplittingStack;
+        ItemStack itemstack1 = this.menu.getCarried();
+        String s = null;
+        if (pSlot == this.clickedSlot && !this.draggingItem.isEmpty() && this.isSplittingStack && !itemstack.isEmpty()) {
+            itemstack = itemstack.copyWithCount(itemstack.getCount() / 2);
+        } else if (this.isQuickCrafting && this.quickCraftSlots.contains(pSlot) && !itemstack1.isEmpty()) {
+            if (this.quickCraftSlots.size() == 1) {
+                return;
+            }
+
+            if (AbstractContainerMenu.canItemQuickReplace(pSlot, itemstack1, true) && this.menu.canDragTo(pSlot)) {
+                flag = true;
+                int k = Math.min(itemstack1.getMaxStackSize(), pSlot.getMaxStackSize(itemstack1));
+                int l = pSlot.getItem().isEmpty() ? 0 : pSlot.getItem().getCount();
+                int i1 = AbstractContainerMenu.getQuickCraftPlaceCount(this.quickCraftSlots, this.quickCraftingType, itemstack1) + l;
+                if (i1 > k) {
+                    i1 = k;
+                    String var10000 = ChatFormatting.YELLOW.toString();
+                    s = var10000 + k;
+                }
+
+                itemstack = itemstack1.copyWithCount(i1);
+            } else {
+                this.quickCraftSlots.remove(pSlot);
+                this.recalculateQuickCraftRemaining();
+            }
+        }
+
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().translate(0.0F, 0.0F, 100.0F);
+
+        if (!flag1) {
+            if (flag) {
+                pGuiGraphics.fill(x, y, x + 16, y + 16, -2130706433);
+            }
+
+            pGuiGraphics.renderItem(itemstack, x, y, x + y * this.imageWidth);
+            pGuiGraphics.renderItemDecorations(this.font, itemstack, x, y, s);
+        }
+
+        pGuiGraphics.pose().popPose();
+    }
+
+    public void drawCenteredString(GuiGraphics graphics, Font pFont, Component pText, int pX, int pY, int pColor, boolean dropShadow) {
+        FormattedCharSequence formattedcharsequence = pText.getVisualOrderText();
+        this.drawString(graphics, pFont, formattedcharsequence, pX - pFont.width(formattedcharsequence) / 2f, pY, pColor, dropShadow);
+    }
+
+    public void drawString(GuiGraphics graphics, Font font, FormattedCharSequence charSequence, float textX, float textY, int color, boolean dropShadow) {
+        PoseStack poseStack = graphics.pose();
+        MultiBufferSource bufferSource = graphics.bufferSource();
+
+        font.drawInBatch(charSequence, textX, textY, color, dropShadow, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, 15728880);
     }
 }
