1ea60a5b8554bb38bb4a7001f9d2269920be0548
SGUDestiny <panormov2006@gmail.com>
2026-05-18 17:29:00 +0400

    Fixed dice rendering

--- src/main/java/destiny/penumbra_phantasm/client/render/blockentity/ScarletMarbleDiceBlockEntityRenderer.java
+++ src/main/java/destiny/penumbra_phantasm/client/render/blockentity/ScarletMarbleDiceBlockEntityRenderer.java
@@ -1,11 +1,13 @@
 package destiny.penumbra_phantasm.client.render.blockentity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import destiny.penumbra_phantasm.server.block.entity.ScarletMarbleDiceBlockEntity;
 import destiny.penumbra_phantasm.server.registry.BlockRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.resources.model.BakedModel;
@@ -36,11 +38,14 @@ public class ScarletMarbleDiceBlockEntityRenderer implements BlockEntityRenderer
 
         BlockState state = BlockRegistry.SCARLET_MARBLE_DICE_GHOST.get().defaultBlockState();
 
+        RenderType renderType = RenderType.solid();
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(renderType);
         BakedModel model = Minecraft.getInstance().getBlockRenderer().getBlockModel(state);
-        for (net.minecraft.client.renderer.RenderType rt : model.getRenderTypes(state, RandomSource.create(42), ModelData.EMPTY)) {
-            Minecraft.getInstance().getBlockRenderer().renderBatched(state, scarletMarbleDiceBlock.getBlockPos(), level, poseStack,
-                    bufferSource.getBuffer(rt), true, level.getRandom(), ModelData.EMPTY, rt);
-        }
+
+        Minecraft.getInstance().getBlockRenderer().getModelRenderer().renderModel(
+                poseStack.last(), vertexConsumer, state, model,
+                1.0f, 1.0f, 1.0f, packedLight, packedOverlay
+        );
 
         poseStack.popPose();
     }
